d948c0b8c586e6a042fbc1ce20daebdd412e3fc6
jonas_y_issa <jonas.y.issa@gmail.com>
2022-09-30 00:30:41 +1000

    Testing Spinning cube

---
+++ MyWebsite/myapp/static/3dCube.js
@@ -0,0 +1,15 @@
+myMove();
+
+var id = null;
+function myMove() {
+var elem = document.getElementById("rectw");
+var pos = 0;
+clearInterval(id);
+id = setInterval(frame, 10);
+function frame() {
+
+    pos++;
+    $("#rectw").css({'transform': `rotateX(${pos/2}deg)`});
+
+}
+}
