923c1c7515f6f6a58fcc9af9cf458d473e416aa0
jonas_y_issa <jonas.y.issa@gmail.com>
2022-10-11 22:18:09 +1100

    Added favicon view

--- MyWebsite/myapp/views.py
+++ MyWebsite/myapp/views.py
@@ -9,3 +9,15 @@ def index(request):
 def book_by_id(request, book_id):
     book = Book.objects.get(pk=book_id)
     return render(request, 'book_details.html', {'book':book})
+
+from django.conf import settings
+from django.http import FileResponse, HttpRequest, HttpResponse
+from django.views.decorators.cache import cache_control
+from django.views.decorators.http import require_GET
+
+
+@require_GET
+@cache_control(max_age=60 * 60 * 24, immutable=True, public=True)  # one day
+def favicon(request: HttpRequest) -> HttpResponse:
+    file = (settings.BASE_DIR / "static" / "favicon.svg").open("rb")
+    return FileResponse(file)
