084217a2301d84814a3f5018c65d100ddb3087d8
jonas_y_issa <jonas.y.issa@gmail.com>
2022-10-12 13:20:20 +1100

    Added If else statement for Development vs debug mode

--- MyWebsite/MyWebsite/settings.py
+++ MyWebsite/MyWebsite/settings.py
@@ -25,7 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-mn**v-knmxin$nj(+9cp(6a(k91^czc1l_klm#cl&onit_y7(@'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+if 'nimbus' in str(BASE_DIR):
+    DEBUG = False
+else:
+    DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
